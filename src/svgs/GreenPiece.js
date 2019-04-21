998b2205e17027fa79bb2901a5b255db52a6d8b2
QMaximillian <quinnlashinsky@gmail.com>
2019-04-21 18:38:58 -0400

    Fix more styles related to the SVG buttons

--- src/svgs/GreenPiece.js
+++ src/svgs/GreenPiece.js
@@ -14,20 +14,11 @@ export const GreenPiece = ({ sound, handleClick, lightUp, playMode }) => {
         lightUp ? setPlaying(true) : setPlaying(false)
     })
 
-    // return (
-
-    //     <svg pointerEvents="none" width="241" height="238" viewBox="0 0 241 238" fill="none">
-
-    //     <path onClick={playMode ? (event) => handleClick(Number(event.target.dataset.id)) : null} data-id="1" pointerEvents="all" fill="lime"
-    //                 stroke={lightUp ? "gold" : "black"} strokeMiterlimit="10" strokeWidth="8px"/>
-    //     </svg>
-
     return (
         <svg pointerEvents="none" width="252" height="251" viewBox="0 0 252 251" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path onClick={playMode ? (event) => handleClick(Number(event.target.dataset.id)) : null} data-id="1" pointerEvents="all"
                 stroke={lightUp ? "gold" : "black"} strokeMiterlimit="10" strokeWidth={lightUp ? "8px" : "1px" }
                 d="M241.296 11V145.167C188.641 145.167 145.999 187.267 145.999 239.254H10C10 113.273 113.588 11 241.296 11Z" fill="lime" />
-
         </svg>
     )
 }
--- src/svgs/RedPiece.js
+++ src/svgs/RedPiece.js
@@ -8,14 +8,7 @@ export const RedPiece = ({ handleClick, lightUp, playMode, sound }) => {
 
     useEffect(() => { 
         lightUp ? setPlaying(true) : setPlaying(false);
-    });    
-
-    // return (
-    //     <svg pointerEvents="none" width="252" height="251" viewBox="0 0 252 251" fill="none" xmlns="http://www.w3.org/2000/svg">
-    //         <path onClick={playMode ? (event) => handleClick(Number(event.target.dataset.id)) : null} data-id="2" pointerEvents="all" d="M9.5 12V146.167C62.1554 146.167 104.797 188.267 104.797 240.254H240.796C240.796 114.273 137.208 12 9.5 12Z" fill="red" stroke={lightUp ? "gold" : "black"} strokeMiterlimit="10" strokeWidth="8px" />
-    //         <path d="M9.5 12V11.5H9V12H9.5ZM9.5 146.167H9V146.667H9.5V146.167ZM104.797 240.254H104.297V240.754H104.797V240.254ZM240.796 240.254V240.754H241.296V240.254H240.796ZM9 12V146.167H10V12H9ZM9.5 146.667C61.8853 146.667 104.297 188.549 104.297 240.254H105.297C105.297 187.985 62.4255 145.667 9.5 145.667V146.667ZM104.797 240.754H240.796V239.754H104.797V240.754ZM241.296 240.254C241.296 113.991 137.478 11.5 9.5 11.5V12.5C136.938 12.5 240.296 114.556 240.296 240.254H241.296Z" fill="black" />
-    //     </svg>
-    // )
+    });
 
     return (
     <svg 
